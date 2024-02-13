9a13107764a711834b30434777f2a90652d293c8
Joselu <joseluissgz97@gmail.com>
2024-02-13 17:48:18 +0100

    typing loader

---
+++ src/app/presentation/components/chat-loader/chat-loader.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'chat-loader',
+  standalone: true,
+  imports: [],
+  templateUrl: './chat-loader.component.html',
+  styleUrl: './chat-loader.component.css',
+})
+export class ChatLoaderComponent {}
