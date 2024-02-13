e0f682f82e56ca1a048b9662d0f674911e94af02
Joselu <joseluissgz97@gmail.com>
2024-02-13 17:47:14 +0100

    routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,125 @@
 import { Routes } from '@angular/router';
+import { DashBoardLayoutComponent } from './presentation/layouts/dashBoardLayout/dashBoardLayout.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: DashBoardLayoutComponent,
+    children: [
+      {
+        path: 'orthography',
+        loadComponent: () =>
+          import(
+            './presentation/pages/orthographyPage/orthographyPage.component'
+          ).then((m) => m.OrthographyPageComponent),
+        data: {
+          icon: 'fa-solid fa-spell-check',
+          title: 'Ortografía',
+          description: 'Corregir ortografía',
+        },
+      },
+      {
+        path: 'pros-cons',
+        loadComponent: () =>
+          import(
+            './presentation/pages/prosConsPage/prosConsPage.component'
+          ).then((m) => m.ProsConsPageComponent),
+        data: {
+          icon: 'fa-solid fa-code-compare',
+          title: 'Pros & Cons',
+          description: 'Comparar pros y contras',
+        },
+      },
+      {
+        path: 'pros-cons-stream',
+        loadComponent: () =>
+          import(
+            './presentation/pages/prosConsStreamPage/prosConsStreamPage.component'
+          ).then((m) => m.ProsConsStreamPageComponent),
+        data: {
+          icon: 'fa-solid fa-water',
+          title: 'Como stream',
+          description: 'Con stream de mensajes',
+        },
+      },
+      {
+        path: 'translate',
+        loadComponent: () =>
+          import(
+            './presentation/pages/translatePage/translatePage.component'
+          ).then((m) => m.TranslatePageComponent),
+        data: {
+          icon: 'fa-solid fa-language',
+          title: 'Traducir',
+          description: 'Textos a otros idiomas',
+        },
+      },
+      {
+        path: 'text-to-audio',
+        loadComponent: () =>
+          import(
+            './presentation/pages/textToAudioPage/textToAudioPage.component'
+          ).then((m) => m.TextToAudioPageComponent),
+        data: {
+          icon: 'fa-solid fa-podcast',
+          title: 'Texto a audio',
+          description: 'Convertir texto a audio',
+        },
+      },
+      {
+        path: 'audio-to-text',
+        loadComponent: () =>
+          import(
+            './presentation/pages/audioToTextPage/audioToTextPage.component'
+          ).then((m) => m.AudioToTextPageComponent),
+        data: {
+          icon: 'fa-solid fa-comment-dots',
+          title: 'Audio a texto',
+          description: 'Convertir audio a texto',
+        },
+      },
+      {
+        path: 'image-generation',
+        loadComponent: () =>
+          import(
+            './presentation/pages/imageGenerationPage/imageGenerationPage.component'
+          ).then((m) => m.ImageGenerationPageComponent),
+        data: {
+          icon: 'fa-solid fa-image',
+          title: 'Imágenes',
+          description: 'Generar imágenes',
+        },
+      },
+      {
+        path: 'image-tunning',
+        loadComponent: () =>
+          import(
+            './presentation/pages/imageTunningPage/imageTunningPage.component'
+          ).then((m) => m.ImageTunningPageComponent),
+        data: {
+          icon: 'fa-solid fa-wand-magic',
+          title: 'Editar imagen',
+          description: 'Generación continua',
+        },
+      },
+
+      {
+        path: 'assistant',
+        loadComponent: () =>
+          import(
+            './presentation/pages/assistantPage/assistantPage.component'
+          ).then((m) => m.AssistantPageComponent),
+        data: {
+          icon: 'fa-solid fa-user',
+          title: 'Asistente',
+          description: 'Información del asistente',
+        },
+      },
+      {
+        path: '**',
+        redirectTo: 'orthography',
+        pathMatch: 'full',
+      },
+    ],
+  },
+];
