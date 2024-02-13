5d500279465e90ad0c0ad200186311bb630457b6
Joselu <joseluissgz97@gmail.com>
2024-02-13 17:47:39 +0100

    barril to export all components

---
+++ src/app/presentation/components/index.ts
@@ -0,0 +1,6 @@
+export * from './chat-bubbles/gptMessage/gptMessage.component';
+export * from './chat-bubbles/myMessage/myMessage.component';
+
+export * from './nav-item/nav-item.component';
+
+export * from './chat-loader/chat-loader.component';
