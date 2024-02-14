58f30c38dbea9ef4f39f52c6e58ce101867396d9
Joselu <joseluissgz97@gmail.com>
2024-02-14 12:51:26 +0100

    text-message chat component

---
+++ src/app/presentation/components/text-boxes/textMessageBox/textMessageBox.component.ts
@@ -0,0 +1,38 @@
+import { CommonModule } from '@angular/common';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  EventEmitter,
+  Input,
+  Output,
+  inject,
+} from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'text-message-box',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
+  templateUrl: './textMessageBox.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class TextMessageBoxComponent {
+  @Input() placeholder: string = '';
+  @Input() disableCorrection: boolean = false;
+
+  @Output() onSendMessage = new EventEmitter<string>();
+
+  private fb = inject(FormBuilder);
+
+  public myForm = this.fb.group({
+    prompt: ['', Validators.required],
+  });
+
+  onSubmit() {
+    if (this.myForm.invalid) return;
+
+    const { prompt } = this.myForm.value;
+    this.onSendMessage.emit(prompt ? prompt : '');
+    this.myForm.reset();
+  }
+}
