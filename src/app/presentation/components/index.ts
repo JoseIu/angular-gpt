427e52afadbd8ec2d0b2512278c79cf940bbdd07
Joselu <joseluissgz97@gmail.com>
2024-02-14 12:51:47 +0100

    export text message component

--- src/app/presentation/components/index.ts
+++ src/app/presentation/components/index.ts
@@ -4,3 +4,5 @@ export * from './chat-bubbles/myMessage/myMessage.component';
 export * from './nav-item/nav-item.component';
 
 export * from './chat-loader/chat-loader.component';
+
+export * from './text-boxes/textMessageBox/textMessageBox.component';
