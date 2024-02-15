2abb1c459a648c547fad0ea55689f24862949e5b
Joselu <joseluissgz97@gmail.com>
2024-02-15 18:27:15 +0100

    chat template

---
+++ src/app/template/chatTemplade/chatTemplade.component.ts
@@ -0,0 +1,41 @@
+import { CommonModule } from '@angular/common';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  inject,
+  signal,
+} from '@angular/core';
+import {
+  ChatLoaderComponent,
+  GptMessageComponent,
+  MyMessageComponent,
+  TextMessageBoxComponent,
+} from '@components/index';
+import { Message } from '@interfaces/mesaage.interface';
+import { OpenAiServiceService } from 'app/presentation/services/open-ai-service.service';
+
+@Component({
+  selector: 'chat-templade',
+  standalone: true,
+  imports: [
+    CommonModule,
+    GptMessageComponent,
+    MyMessageComponent,
+    ChatLoaderComponent,
+    TextMessageBoxComponent,
+  ],
+  templateUrl: './chatTemplade.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ChatTempladeComponent {
+  private openAiSerice = inject(OpenAiServiceService);
+  public messages = signal<Message[]>([
+    { message: 'Hello', isGpt: true },
+    { message: 'Hello', isGpt: false },
+  ]);
+  public isloading = signal(false);
+
+  public getMessage(event: string) {
+    console.log(event);
+  }
+}
