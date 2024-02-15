4d704f1c667648bafcd7b32a8ef1c576506c839e
Joselu <joseluissgz97@gmail.com>
2024-02-15 18:12:50 +0100

    Barril: textMessageBoxSelect, textMessageFile

--- src/app/presentation/components/index.ts
+++ src/app/presentation/components/index.ts
@@ -6,3 +6,5 @@ export * from './nav-item/nav-item.component';
 export * from './chat-loader/chat-loader.component';
 
 export * from './text-boxes/textMessageBox/textMessageBox.component';
+export * from './text-boxes/textMessageBoxSelect/textMessageBoxSelect.component';
+export * from './text-boxes/textMessageFile/textMessageFile.component';
