4dff4ce610fa35cda1264b8cb179a0453ef95075
Joselu <joseluissgz97@gmail.com>
2024-02-15 18:26:56 +0100

    importing openAiSerice and removing components that not using

--- src/app/presentation/pages/orthographyPage/orthographyPage.component.ts
+++ src/app/presentation/pages/orthographyPage/orthographyPage.component.ts
@@ -1,10 +1,19 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  inject,
+  signal,
+} from '@angular/core';
 import {
   ChatLoaderComponent,
   GptMessageComponent,
   MyMessageComponent,
+  TextMessageBoxSelectComponent,
 } from '@components/index';
+import { Message } from '@interfaces/mesaage.interface';
+import { TextMessageBoxSelect } from '@interfaces/text.MessageSelect.interface';
+import { OpenAiServiceService } from 'app/presentation/services/open-ai-service.service';
 
 @Component({
   selector: 'app-orthography-page',
@@ -14,8 +23,19 @@ import {
     GptMessageComponent,
     MyMessageComponent,
     ChatLoaderComponent,
+    TextMessageBoxSelectComponent,
   ],
   templateUrl: './orthographyPage.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class OrthographyPageComponent {}
+export class OrthographyPageComponent {
+  private openAiSerice = inject(OpenAiServiceService);
+  public messages = signal<Message[]>([
+    { message: 'Hello', isGpt: true },
+    { message: 'Hello', isGpt: false },
+  ]);
+  public isloading = signal(false);
+  public getMessage(event: TextMessageBoxSelect) {
+    console.log(event);
+  }
+}
