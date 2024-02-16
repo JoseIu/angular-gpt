25b53fbc55b03e0a46d261c10ee59695b6c2c55f
Joselu <joseluissgz97@gmail.com>
2024-02-16 17:57:35 +0100

    barril interfaces

--- src/app/interfaces/index.ts
+++ src/app/interfaces/index.ts
@@ -2,3 +2,5 @@ export * from './mesaage.interface';
 export * from './nav.interface';
 export * from './text.MessageSelect.interface';
 export * from './textMessage.interface';
+
+export * from './fetch/ortographyResponse.intertface';
