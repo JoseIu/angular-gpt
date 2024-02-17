a1f8905a6cb3b53906c6fe2345f7b7cc268741c7
Joselu <joseluissgz97@gmail.com>
2024-02-17 18:52:49 +0100

    ProsConsPage

--- src/app/presentation/pages/prosConsPage/prosConsPage.component.ts
+++ src/app/presentation/pages/prosConsPage/prosConsPage.component.ts
@@ -1,11 +1,63 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  inject,
+  signal,
+} from '@angular/core';
+import {
+  ChatLoaderComponent,
+  GptMessageComponent,
+  MyMessageComponent,
+  TextMessageBoxComponent,
+} from '@components/index';
+import { Message } from '@interfaces/mesaage.interface';
+import { OpenAiServiceService } from 'app/presentation/services/open-ai-service.service';
 
 @Component({
   selector: 'app-pros-cons-page',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    GptMessageComponent,
+    MyMessageComponent,
+    ChatLoaderComponent,
+    TextMessageBoxComponent,
+  ],
   templateUrl: './prosConsPage.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ProsConsPageComponent {}
+export class ProsConsPageComponent {
+  private openAiSerice = inject(OpenAiServiceService);
+
+  public messages = signal<Message[]>([]);
+
+  public isloading = signal(false);
+
+  public getMessage(prompt: string) {
+    console.log(prompt);
+    this.isloading.set(true);
+
+    //Our messages
+    this.messages.update((previusMessages) => [
+      ...previusMessages,
+      {
+        isGpt: false,
+        text: prompt,
+      },
+    ]);
+
+    this.openAiSerice.prosCons(prompt).subscribe((resp) => {
+      this.isloading.set(false);
+
+      //OpenAI messages
+      this.messages.update((previusMessages) => [
+        ...previusMessages,
+        {
+          isGpt: true,
+          text: resp.content,
+        },
+      ]);
+    });
+  }
+}
