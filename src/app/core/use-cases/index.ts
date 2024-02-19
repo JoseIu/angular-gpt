1bc65719b164c163c126cc4b71f541269ea0701d
Joselu <joseluissgz97@gmail.com>
2024-02-19 11:06:24 +0100

    barril: translate.use-case

--- src/app/core/use-cases/index.ts
+++ src/app/core/use-cases/index.ts
@@ -3,3 +3,5 @@ export * from './orthography/orthography.use-case';
 export * from './pros-cons/prosCons.use-case';
 
 export * from './pros-cons-stream/prosConsStream.use-case';
+
+export * from './translate/translate.use-case';
