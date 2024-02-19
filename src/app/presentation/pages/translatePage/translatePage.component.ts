16592234c55c5a3c0a40c7cdbc2608b45b710bfc
Joselu <joseluissgz97@gmail.com>
2024-02-19 11:08:06 +0100

    translate Page

--- src/app/presentation/pages/translatePage/translatePage.component.ts
+++ src/app/presentation/pages/translatePage/translatePage.component.ts
@@ -1,13 +1,64 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  inject,
+  signal,
+} from '@angular/core';
+import {
+  ChatLoaderComponent,
+  GptMessageComponent,
+  MyMessageComponent,
+  TextMessageBoxSelectComponent,
+} from '@components/index';
+import { Message } from '@interfaces/mesaage.interface';
+import {
+  Option,
+  TextMessageBoxSelect,
+} from '@interfaces/text.MessageSelect.interface';
+import { OpenAiServiceService } from 'app/presentation/services/open-ai-service.service';
 
 @Component({
   selector: 'app-translate-page',
   standalone: true,
   imports: [
     CommonModule,
+    GptMessageComponent,
+    MyMessageComponent,
+    ChatLoaderComponent,
+    TextMessageBoxSelectComponent,
   ],
   templateUrl: './translatePage.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class TranslatePageComponent { }
+export class TranslatePageComponent {
+  public options: Option[] = [
+    { id: 'alemán', text: 'Alemán' },
+    { id: 'árabe', text: 'Árabe' },
+    { id: 'bengalí', text: 'Bengalí' },
+    { id: 'francés', text: 'Francés' },
+    { id: 'hindi', text: 'Hindi' },
+    { id: 'inglés', text: 'Inglés' },
+    { id: 'japonés', text: 'Japonés' },
+    { id: 'mandarín', text: 'Mandarín' },
+    { id: 'portugués', text: 'Portugués' },
+    { id: 'ruso', text: 'Ruso' },
+  ];
+  private openAiSerice = inject(OpenAiServiceService);
+
+  public messages = signal<Message[]>([]);
+
+  public isloading = signal(false);
+
+  public getMessage({ prompt, lang }: TextMessageBoxSelect) {
+    const message = `Traduce a ${lang}: ${prompt}`;
+
+    this.isloading.set(true);
+    this.messages.update((prev) => [...prev, { text: message, isGpt: false }]);
+
+    this.openAiSerice.translate(prompt, lang).subscribe(({ message }) => {
+      this.isloading.set(false);
+      this.messages.update((prev) => [...prev, { text: message, isGpt: true }]);
+    });
+  }
+}
