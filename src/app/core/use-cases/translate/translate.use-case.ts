42c0dc46c5c9baf2a2b3872eddd7bae73b203505
Joselu <joseluissgz97@gmail.com>
2024-02-19 11:06:30 +0100

    translateUseCase

---
+++ src/app/core/use-cases/translate/translate.use-case.ts
@@ -0,0 +1,32 @@
+import { Translate } from '@interfaces/fetch/translate.interface';
+import { environment } from 'environments/environment';
+
+export const translateUseCase = async (prompt: string, lang: string) => {
+  try {
+    console.log({ prompt, lang });
+
+    const response = await fetch(`${environment.backendUrl}/translate`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ prompt, lang }),
+    });
+
+    if (!response.ok) throw new Error('NO se pudo realizar la tradcución');
+
+    const { message }: Translate = await response.json();
+
+    console.log(message);
+
+    // console.log({ data });
+
+    return {
+      ok: true,
+      message,
+    };
+  } catch (error) {
+    console.log(error);
+    throw new Error('Problemas en la tradcución');
+  }
+};
