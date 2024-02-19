fcc978e6ada2e8eb83529a542d239d1a601ae3e8
Joselu <joseluissgz97@gmail.com>
2024-02-19 18:14:20 +0100

    Barril: textToAudio.use-case

--- src/app/core/use-cases/index.ts
+++ src/app/core/use-cases/index.ts
@@ -5,3 +5,5 @@ export * from './pros-cons/prosCons.use-case';
 export * from './pros-cons-stream/prosConsStream.use-case';
 
 export * from './translate/translate.use-case';
+
+export * from './textToAudio/textToAudio.use-case';
