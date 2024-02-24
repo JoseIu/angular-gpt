e15d3833d5b9444f25c2c019d881636596a55dc1
Joselu <joseluissgz97@gmail.com>
2024-02-24 13:31:22 +0100

    imagePageGeneration

--- src/app/presentation/pages/imageGenerationPage/imageGenerationPage.component.ts
+++ src/app/presentation/pages/imageGenerationPage/imageGenerationPage.component.ts
@@ -1,13 +1,57 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  inject,
+  signal,
+} from '@angular/core';
+import {
+  ChatLoaderComponent,
+  GptMessageComponent,
+  MyMessageComponent,
+  TextMessageBoxComponent,
+} from '@components/index';
+import { Message } from '@interfaces/mesaage.interface';
+import { OpenAiServiceService } from 'app/presentation/services/open-ai-service.service';
 
 @Component({
   selector: 'app-image-generation-page',
   standalone: true,
   imports: [
     CommonModule,
+    GptMessageComponent,
+    MyMessageComponent,
+    ChatLoaderComponent,
+    TextMessageBoxComponent,
   ],
   templateUrl: './imageGenerationPage.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ImageGenerationPageComponent { }
+export class ImageGenerationPageComponent {
+  private openAiSerice = inject(OpenAiServiceService);
+
+  public messages = signal<Message[]>([]);
+
+  public isloading = signal(false);
+
+  public getMessage(prompt: string) {
+    this.messages.update((previusMessage) => [
+      ...previusMessage,
+      {
+        isGpt: false,
+        text: prompt,
+      },
+    ]);
+    this.openAiSerice.imageGeneration(prompt).subscribe((resp) => {
+      this.isloading.set(false);
+      this.messages.update((previusMessage) => [
+        ...previusMessage,
+        {
+          isGpt: true,
+          text: 'Imagen generada:',
+          imageUrl: resp.imageUrl,
+        },
+      ]);
+    });
+  }
+}
