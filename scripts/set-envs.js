15cc1ad44440e3d7311f181ff0c7e5a138d36f25
Joselu <joseluissgz97@gmail.com>
2024-02-29 13:24:32 +0100

    Script environment

--- scripts/set-envs.js
+++ scripts/set-envs.js
@@ -5,7 +5,7 @@ const { writeFileSync, mkdirSync } = require("fs");
 const targetPath = "./src/environments/environment.ts";
 const envFileContent = `
     export const environment = {
-      backendUrl:"${process.env.BACK_URL}",
+      backendUrl:"${process.env.BACK_URL}/gpt",
     };
   `;
 
